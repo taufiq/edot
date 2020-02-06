020e9b13da8ee30dce71c5fab87816cc539cb770
Taufiq Mohammed <taufiqmmhd@gmail.com>
2020-02-06 12:09:38 +0800

    feat: add nurse notification screen

--- frontend/src/screens/Landing.js
+++ frontend/src/screens/Landing.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../svg/landing-person.svg';
-import { ReactComponent as Background } from '../svg/landing-background.svg';
+import { ReactComponent as LandingHero } from '../svg/landing-person.svg';
+import { ReactComponent as BlackBackground } from '../svg/landing-background.svg';
+import { ReactComponent as WhiteBackground } from '../svg/landing-background-white.svg';
+import { ReactComponent as NotifyHero } from '../svg/landing-notify.svg';
 
 export default class LandingScreen extends Component {
   constructor(props) {
@@ -35,7 +37,15 @@ export default class LandingScreen extends Component {
     }
     return (
       <div className="h-100 d-flex flex-column justify-content-center align-items-center">
-        <div className="position-absolute background"><Background /></div>
+        <div
+          className="position-absolute background"
+          style={{ backgroundColor: showingNotifyScreen && '#85A458'  }}>
+            { showingNotifyScreen
+              ? <WhiteBackground />
+              : <BlackBackground />
+            }
+          </div>
+        <p className="position-absolute pt-3 pr-1 font-weight-bold" style={{ top: '0', right: '0', color: showingNotifyScreen && '#fff' }}>9/10 days left</p>
         { showingNotifyScreen
             ? <NotifyScreen/>
             : (
@@ -61,7 +71,7 @@ export default class LandingScreen extends Component {
 
 const SideEffectsCard = ({ onCheck, checked }) => (
   <>
-    <Logo />
+    <LandingHero />
     <h4>John Choo</h4>
     {/* <p>S8888888A</p> */}
     <div className="card w-75">
@@ -104,6 +114,10 @@ const InstructionsCard = () => {
 
 const NotifyScreen = () => {
   return (
-    <p>John Choo</p>
+    <div className="d-flex flex-column align-items-center">
+      <NotifyHero />
+      <h4 className="mt-3" style={{ color: 'white' }}>John Choo</h4>
+      <p className="text-center" style={{ color: 'white' }}>We notified the nurse. <br />Please wait for the call.</p>
+    </div>
   )
 }
